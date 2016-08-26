94d74e92ddccf8d91cf4e8362c9b78e9eaddf22a
Rose Ryan Campau <rcampau@icloud.com>
2016-08-26 19:00:32 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz(int)
+  result = ""
+  if int % 3 == 0 || int % 5 == 0
+    if int % 3 == 0
+      result += "Fizz"
+
+    end
+    if int % 5 == 0
+      result += "Buzz"
+    end
+    return result
+  else
+    return nil
+  end
+
+end
